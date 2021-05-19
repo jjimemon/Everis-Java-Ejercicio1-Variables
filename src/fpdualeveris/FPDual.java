84fe7f5df3b55c9b8505e3f7ac301d9f406d0028
jjimemon <jjimemon@everis.com>
2021-05-19 12:23:34 +0200

    Segundo commit : código refactorizado y funciones extras añadidas

--- src/fpdualeveris/FPDual.java
+++ src/fpdualeveris/FPDual.java
@@ -19,44 +19,69 @@ public class FPDual {
 
 	public static void main(String[] args) {
 
-		// Llamamiento al método.
+		// Llamamiento al método variablesChallenge.
 		variablesChallenge();
 
 	}
 
-	// Método que crea los objeto de tipo Developer y el uso de sus métodos.
+	/**
+	 * Method that creates the objects of type Developer and the use of its methods
+	 */
 	private static void variablesChallenge() {
 
-		// Creaciones de objetos.
-		Developer dev1 = new Developer("Manuel");
-		Developer dev2 = new Developer("Javier");
-		Developer dev3 = new Developer("Alvaro");
-		Developer dev4 = new Developer("Raquel");
+		// Creador del objeto StringBuilder
+		StringBuilder cadena = new StringBuilder();
 
-		// Nos devuelve el valor total de empleados en la empresa.
-		System.out.println("Total de empleados en la empresa llamada desde el empleado con ID " + dev1.getIdEmployed() + " es: " + dev1.getEmployed());
-		System.out.println("Total de empleados en la empresa llamada desde el empleado con ID " + dev4.getIdEmployed() + " es: " + dev4.getEmployed());
+		// Creador de objetos Developer.
+		if (Developer.verification(4)) {
+			Developer dev1 = new Developer("Manuel");
+			Developer dev2 = new Developer("Javier");
+			Developer dev3 = new Developer("Alvaro");
+			Developer dev4 = new Developer("Raquel");
+		
+			// Devuelve el valor total de empleados en la empresa llamado desde diferentes desarrolladores.
+			System.out.println(cadena.append("Total de empleados en la empresa llamada desde el empleado con ID ").append(dev1.getIdEmployed()).append(" es: ")
+			        .append(dev1.getEmployed()));
+			cadena.setLength(0);
 
-		// Devuelve el id de cada empleado
-		System.out.println("\n" + "el empleado " + dev1.getEmployedName() + " posee la ID " + dev1.getIdEmployed());
-		System.out.println("el empleado " + dev2.getEmployedName() + " posee la ID " + dev2.getIdEmployed());
-		System.out.println("el empleado " + dev3.getEmployedName() + " posee la ID " + dev3.getIdEmployed());
-		System.out.println("el empleado " + dev4.getEmployedName() + " posee la ID " + dev4.getIdEmployed());
+			System.out.println(cadena.append("Total de empleados en la empresa llamada desde el empleado con ID ").append(dev4.getIdEmployed()).append(" es: ")
+			        .append(dev4.getEmployed()));
+			cadena.setLength(0);
 
-		// Invocacion del metodo que resta dias de vacaciones al objeto.
+			// Devuelve el id de cada empleado
+			System.out.println(cadena.append("\n").append("el empleado ").append(dev1.getEmployedName()).append(" posee la ID ").append(dev1.getIdEmployed()));
+			cadena.setLength(0);
+			System.out.println(cadena.append("el empleado ").append(dev2.getEmployedName()).append(" posee la ID ").append(dev2.getIdEmployed()));
+			cadena.setLength(0);
+			System.out.println(cadena.append("el empleado ").append(dev3.getEmployedName()).append(" posee la ID ").append(dev3.getIdEmployed()));
+			cadena.setLength(0);
+			System.out.println(cadena.append("el empleado ").append(dev4.getEmployedName()).append(" posee la ID ").append(dev4.getIdEmployed()));
+			cadena.setLength(0);
 
-		System.out.println("\n" + "Los días actuales de vacaciones de " + dev2.getEmployedName() + " es de " + dev2.getHolidaysEmployed() + " tras la resta indicada, ahora es de "
-		        + dev2.restarDiasVacaciones(3));
+			// Invocacion del método que resta días de vacaciones al objeto.
 
-		// Consulta de días que puede disfrutar cada programador.
-		System.out.println("\n" + "Los dias actuales de vacaciones del empleado "+dev1.getEmployedName()+" es de "+dev1.getHolidaysEmployed());
-		System.out.println("Los dias actuales de vacaciones del empleado "+dev2.getEmployedName()+" es de "+dev2.getHolidaysEmployed());
-		System.out.println("Los dias actuales de vacaciones del empleado "+dev3.getEmployedName()+" es de "+dev3.getHolidaysEmployed());
-		System.out.println("Los dias actuales de vacaciones del empleado "+dev4.getEmployedName()+" es de "+dev4.getHolidaysEmployed()+"\n");
+			System.out.println(cadena.append("\n").append("Los días actuales de vacaciones de ").append(dev2.getEmployedName()).append(" es de ")
+			        .append(dev2.getHolidaysEmployed()).append(" tras la resta indicada, ahora es de ").append(dev2.subtractVacationDays(3)));
+			cadena.setLength(0);
 
-		// Pinta la empresa, llamandola desde la clase directamente.
-		System.out.println(Developer.EMPRESA);
+			// Consulta de días que puede disfrutar cada programador.
+			System.out.println(cadena.append("\n").append("Los dias actuales de vacaciones del empleado ").append(dev1.getEmployedName()).append(" es de ")
+			        .append(dev1.getHolidaysEmployed()));
+			cadena.setLength(0);
+			System.out.println(cadena.append("Los dias actuales de vacaciones del empleado ").append(dev2.getEmployedName()).append(" es de ")
+			        .append(dev2.getHolidaysEmployed()));
+			cadena.setLength(0);
+			System.out.println(cadena.append("Los dias actuales de vacaciones del empleado ").append(dev3.getEmployedName()).append(" es de ")
+			        .append(dev3.getHolidaysEmployed()));
+			cadena.setLength(0);
+			System.out.println(cadena.append("Los dias actuales de vacaciones del empleado ").append(dev4.getEmployedName()).append(" es de ")
+			        .append(dev4.getHolidaysEmployed()));
+			cadena.setLength(0);
+
+			// Pinta la empresa, llamandola desde la clase directamente.
+			System.out.println(Developer.getBUSINESS());
+
+		}
 
 	}
-
 }
--- src/variables/Developer.java
+++ src/variables/Developer.java
@@ -1,15 +1,27 @@
 package variables;
 
+
+/**
+ * Class that contains all the application logic
+ * 
+ * @author jjimemon
+ *
+ */
+
 public class Developer {
 
-	// Declaraciones de variables.
+	/** Variable declarations */
 	private static int employed = 0;
 	private int idEmployed;
 	private String employedName;
 	private int holidaysEmployed = 21;
-	public final static String EMPRESA = "EVERIS";
+	public final static String BUSINESS = "EVERIS";
 
-	// Método constructor.
+	/**
+	 * Constructor that receives employee name and creates the object
+	 * 
+	 * @param employedName
+	 */
 	public Developer(String employedName) {
 
 		employed++;
@@ -18,17 +30,13 @@ public class Developer {
 
 	}
 
-	// toString con el patron de escritura por consola.
+	/** toString with the console write pattern */
 	@Override
 	public String toString() {
 		return "Developer: EmployedName=" + employedName + ", IDEmployed=" + idEmployed + ", HolidaysEmployed=" + holidaysEmployed + "]";
 	}
 
-	/**
-	 * Getters and Setters.
-	 * 
-	 *
-	 */
+	/** Getters and Setters. */
 	public String getEmployedName() {
 		return employedName;
 	}
@@ -53,27 +61,51 @@ public class Developer {
 		this.holidaysEmployed = holidaysEmployed;
 	}
 
-	public static String getEmpresa() {
-		return EMPRESA;
+	public static String getBUSINESS() {
+		return BUSINESS;
 	}
 
-	// Métodos.
+	/**
+	 * Method that subtracts vacation days from the invoking object
+	 * 
+	 * @param days
+	 * @return resul
+	 */
+	public int subtractVacationDays(int days) {
+		int resul;
 
-	// Método que resta dias de vacaciones al objeto invocante.
-	public int restarDiasVacaciones(int diasARestar) {
-		int resultado;
-		
-		//Condicional que controla que el día introducido sea mayor que 0 y que el empleado tenga mas dias de vacaciones que el numero introducido por parámetro.
-		if(this.getHolidaysEmployed()>=diasARestar && diasARestar>0) {
+		// Condicional que controla que el día introducido sea mayor que 0 y que el empleado tenga mas dias de vacaciones que el numero introducido por
+		// parámetro.
+		if (this.getHolidaysEmployed() >= days && days > 0) {
 			// trae los dias de vacaciones del objeto invocante y lo resta con los días introducidos por parámetros.
-			resultado = this.getHolidaysEmployed() - diasARestar;
-			this.setHolidaysEmployed(resultado);
-		}else {
-			resultado=this.getHolidaysEmployed();
+			resul = this.getHolidaysEmployed() - days;
+			this.setHolidaysEmployed(resul);
+		} else {
+			resul = this.getHolidaysEmployed();
 			System.out.println("Error, no se le puede restar más dias de vacaciones o el valor introducido es menor a 0");
 		}
 
-		return resultado;
+		return resul;
+
+	}
+	
+	/**
+	 * Method that verifies whether or not more employees can be added.
+	 * @param number
+	 * @return yesOrNot
+	 */
+	public static boolean verification(int number) {
+
+		boolean yesOrNot=Boolean.FALSE;
+
+		if(getEmployed()+number>127) {
+			System.out.println("No se ha podido iniciar, se ha excedido del límite de empleados(127)");
+		}else {
+			yesOrNot=Boolean.TRUE;
+		}
+		
+		
+		return yesOrNot;
 
 	}
 
